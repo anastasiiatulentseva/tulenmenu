62a734cc3cf98d2a103ba6b978d8bbf714ae3a56
Anastasiia Tulentseva <nastena-skid@yandex.ru>
2015-12-23 19:10:29 +0300

    add suggested dishes index. layout refactoring

--- test/controllers/suggested_dishes_controller_test.rb
+++ test/controllers/suggested_dishes_controller_test.rb
@@ -2,6 +2,25 @@ require 'test_helper'
 
 class SuggestedDishesControllerTest < ActionController::TestCase
   
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_select "title",  "Suggested dish list | Menu Tulentsevyh"
+  end
+  
+  test "should redirect new for correct suggested dish" do
+    assert_difference 'SuggestedDish.count', 1 do
+      post :create, suggested_dish: { name: "dish", comment: "hey hey" }
+    end
+    assert_redirected_to root_url
+  end
+  
+  test "should render new for wrong suggested dish" do
+    assert_no_difference 'SuggestedDish.count' do
+      post :create, suggested_dish: { name: "d" }
+    end
+    assert_template 'suggested_dishes/new'
+  end
   
   
 end
